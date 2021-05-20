207b74f8ebb558001f7b84b6e4f07aa090c52d67
Michael C. Main <deusprogrammer@gmail.com>
2021-05-20 13:54:14 -0500

    Trying to fix download zip

--- src/routes/VideoList.jsx
+++ src/routes/VideoList.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import {saveAs} from 'file-saver';
 
 let VideoList = () => {
     const [videos, setVideos] = useState([]);
