5d2f734edbff9f024b0e227c5c9756b3a57ffab1
Michael C. Main <deusprogrammer@gmail.com>
2021-05-20 16:25:43 -0500

    Added video details

--- src/routes/VideoView.jsx
+++ src/routes/VideoView.jsx
@@ -14,12 +14,23 @@ let VideoView = (props) => {
         setVideoDetails(result.data);
     }, []);
 
+    let getVideo = async () => {
+        let results = await axios.get(`https://deusprogrammer.com/api/dubs/videos/${videoDetails._id}/mp4`, {
+            headers: {
+                "X-Access-Token": localStorage.getItem("accessToken")
+            }
+        });
+
+        let base64ByteArray = Buffer.from(results.data, 'binary').toString('base64');
+        return `data:video/mp4;base64,${base64ByteArray}`;
+    }
+
     return (
         <div>
             {videoDetails ? <div>
                 <div>Name: {videoDetails.name}</div>
-                <div><video src={`https://deusprogrammer.com/api/dubs/videos/${videoDetails._id}/mp4`} controls/></div>
-                <div>{convertSubtitlesToSrt(videoDetails.subtitles)}</div>
+                <div><video src={} controls/></div>
+                <pre>{convertSubtitlesToSrt(videoDetails.subtitles)}</pre>
             </div> : null}
         </div>
     );
