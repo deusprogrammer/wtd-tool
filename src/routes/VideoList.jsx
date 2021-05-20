deeeca93033c91df30bbf28cae070ddded2bc02d
Michael C. Main <deusprogrammer@gmail.com>
2021-05-20 16:16:24 -0500

    Added video details

--- src/routes/VideoList.jsx
+++ src/routes/VideoList.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {saveAs} from 'file-saver';
 import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
 
 let VideoList = () => {
     const [videos, setVideos] = useState([]);
@@ -30,7 +31,7 @@ let VideoList = () => {
                 return (
                     <div>
                         {video.name}&nbsp;
-                        <button type="button" onClick={() => {navigator.clipboard.writeText(video._id)}}>Get ID</button>
+                        <button type="button" onClick={() => {navigator.clipboard.writeText(video._id);toast("ID Copied!", {type: "info"})}}>Get ID</button>
                         <button type="button" onClick={() => {getZip(video._id, video.name)}}>Download Zip</button>
                         <Link to={`${process.env.PUBLIC_URL}/videos/${video._id}`}><button type="button">Open Details</button></Link>
                     </div>
--- src/routes/VideoView.jsx
+++ src/routes/VideoView.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { convertSubtitlesToSrt } from '../util/VideoTools';
 
 let VideoView = (props) => {
-    const [videoDetails, setVideoDetails] = useState({});
+    const [videoDetails, setVideoDetails] = useState(null);
     useEffect(async () =>{
         let result = await axios.get(`https://deusprogrammer.com/api/dubs/videos/${props.match.params.id}`, {
             headers: {
@@ -16,9 +16,11 @@ let VideoView = (props) => {
 
     return (
         <div>
-            <div>Name: {videoDetails.name}</div>
-            <div><video src={`https://deusprogrammer.com/api/dubs/videos/${videoDetails._id}/mp4`} controls/></div>
-            <div>{convertSubtitlesToSrt(videoDetails.subtitles)}</div>
+            {videoDetails ? <div>
+                <div>Name: {videoDetails.name}</div>
+                <div><video src={`https://deusprogrammer.com/api/dubs/videos/${videoDetails._id}/mp4`} controls/></div>
+                <div>{convertSubtitlesToSrt(videoDetails.subtitles)}</div>
+            </div> : null}
         </div>
     );
 };
